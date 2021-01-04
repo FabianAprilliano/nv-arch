f309e256be000d845f409c986c324a228012fd87
FabianAprilliano <56262167+FabianAprilliano@users.noreply.github.com>
2021-01-04 18:10:11 +0700

    Update vulkan.sh
    shit i forgot

--- vulkan.sh
+++ vulkan.sh
@@ -1,4 +1,4 @@
-# please enable multilib repo in /etc/pacman.d/mirrorlist 
+# please enable multilib repo in /etc/pacman.conf
 #!/bin/bash 
 
 sudo pacman -S nvidia-utils lib32-nvidia-utils nvidia-dkms vulkan-icd-loader lib32-vulkan-icd-loader
